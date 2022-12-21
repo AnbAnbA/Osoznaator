1834dd2716e773ca9364616cb8240405b0670aff
БыковаАА <protocolapogay@gmail.com>
2022-12-21 14:36:31 +0300

    com

--- app/src/main/java/com/example/osoznaator/AddData.java
+++ app/src/main/java/com/example/osoznaator/AddData.java
@@ -26,73 +26,51 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class AddData extends AppCompatActivity {
 
-    ImageView img;
-    Bitmap bitmap=null, b;
-    EditText Name, Country, Year;
+    EditText Cel, DRazum, Prostr, Ocenkaemotion, Emotion, Doi, Think, Time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_data);
-        img = findViewById(R.id.imgPhoto);
-        b = BitmapFactory.decodeResource(AddData.this.getResources(), R.drawable.picture);
-        Name = findViewById(R.id.etName);
-        Country = findViewById(R.id.etCountry);
-        Year = findViewById(R.id.etYear);
-        img.setImageBitmap(b);
-    }
+        Cel = findViewById(R.id.Cel);
+        DRazum = findViewById(R.id.DRazum);
+        Prostr = findViewById(R.id.Prostr);
+        Ocenkaemotion = findViewById(R.id.Ocenkaemotion);
+        Emotion = findViewById(R.id.Emotion);
+        Doi = findViewById(R.id.Doi);
+        Think = findViewById(R.id.Think);
+        Time = findViewById(R.id.Time);
 
-    private final ActivityResultLauncher<Intent> pickImg = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-        if (result.getResultCode() == RESULT_OK) {
-            if (result.getData() != null) {
-                Uri uri = result.getData().getData();
-                try {
-                    InputStream is = getContentResolver().openInputStream(uri);
-                    bitmap = BitmapFactory.decodeStream(is);
-                    img.setImageBitmap(bitmap);
-                } catch (Exception e) {
-                    Log.e(e.toString(), e.getMessage());
-                }
-            }
-        }
-    });
+    }
 
     public void GoViewData(View v){
         startActivity(new Intent(this, MainActivity.class));
     }
 
-    public void SelectPhoto(View v){
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        pickImg.launch(intent);
-    }
 
     public void DataPost(View v){
-        CheckFieldsClass checkFieldsClass = new CheckFieldsClass(AddData.this);
-        if(checkFieldsClass.IsClear(Name.getText().toString(), Country.getText().toString(), Year.getText().toString())) {
-            EncodeImageClass ei = new EncodeImageClass();
-            postData(Name.getText().toString(), Country.getText().toString(), Integer.parseInt(Year.getText().toString()), ei.Image(bitmap), v);
-        }
+            postData(Cel.getText().toString(), DRazum.getText().toString(), Prostr.getText().toString(), Integer.parseInt(Ocenkaemotion.getText().toString()), Emotion.getText().toString(), Doi.getText().toString(), Think.getText().toString(), Time.getText().toString(), v);
+
     }
 
-    private void postData(String name, String country, int year, String image, View v) {
+    private void postData(String cel, String dRazum, String prostr, int ocenkaemotion, String emotion, String doi, String think, String time,  View v) {
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://ngknn.ru:5001/ngknn/РомановаЕА/practika/api/")
+                .baseUrl("https://ngknn.ru:5001/ngknn/БыковаАА/Api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         RetrofitApi retrofitAPI = retrofit.create(RetrofitApi.class);
-        Films modal = new Films(null, name, country, year, image);
-        Call<Films> call = retrofitAPI.createPost(modal);
-        call.enqueue(new Callback<Films>() {
+        Osozn modal = new Osozn(null, cel, dRazum, prostr, ocenkaemotion, emotion, doi, think, time);
+        Call<Osozn> call = retrofitAPI.createPost(modal);
+        call.enqueue(new Callback<Osozn>() {
             @Override
-            public void onResponse(Call<Films> call, Response<Films> response) {
+            public void onResponse(Call<Osozn> call, Response<Osozn> response) {
                 Toast.makeText(AddData.this, "Данные успешно добавлены!", Toast.LENGTH_SHORT).show();
                 GoViewData(v);
             }
 
             @Override
-            public void onFailure(Call<Films> call, Throwable t) {
+            public void onFailure(Call<Osozn> call, Throwable t) {
                 Toast.makeText(AddData.this, "Ошибка", Toast.LENGTH_SHORT).show();
             }
         });
--- app/src/main/java/com/example/osoznaator/UpdateData.java
+++ app/src/main/java/com/example/osoznaator/UpdateData.java
@@ -26,98 +26,76 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class UpdateData extends AppCompatActivity {
     Bundle arg;
-    Films film;
-    EditText Name, Country, Year;
-    ImageView img;
-    Bitmap bitmap=null;
+    Osozn osozn;
+    EditText Cel, DRazum, Prostr, Ocenkaemotion, Emotion, Doi, Think, Time;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_data);
         arg = getIntent().getExtras();
-        film = arg.getParcelable(Films.class.getSimpleName());
-        Name = findViewById(R.id.etName);
-        Country = findViewById(R.id.etCountry);
-        Year = findViewById(R.id.etYear);
-        img = findViewById(R.id.imgPhoto);
-        Name.setText(film.getNameFilm());
-        Country.setText(film.getCountry());
-        Year.setText(Integer.toString(film.getReleaseYear()));
-        DecodeImageClass decodeImageClass = new DecodeImageClass(UpdateData.this);
-        Bitmap userImage = decodeImageClass.getUserImage(film.getImageFilm());
-        img.setImageBitmap(userImage);
-        if(!film.getImageFilm().equals("null")){
-            bitmap = userImage;
-        }
+        osozn = arg.getParcelable(Osozn.class.getSimpleName());
+        Cel = findViewById(R.id.Cel);
+        DRazum = findViewById(R.id.DRazum);
+        Prostr = findViewById(R.id.Prostr);
+        Ocenkaemotion = findViewById(R.id.Ocenkaemotion);
+        Emotion = findViewById(R.id.Emotion);
+        Doi = findViewById(R.id.Doi);
+        Think = findViewById(R.id.Think);
+        Time = findViewById(R.id.Time);
+        Cel.setText(osozn.getCel());
+        DRazum.setText(osozn.getDRazum());
+        Prostr.getText().toString();
+        Ocenkaemotion.setText(Integer.toString(osozn.getOcenkaemotion()));
+        Emotion.getText().toString();
+        Doi.getText().toString();
+        Think.getText().toString();
+        Time.getText().toString();
+
     }
 
-    private final ActivityResultLauncher<Intent> pickImg = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-        if (result.getResultCode() == RESULT_OK) {
-            if (result.getData() != null) {
-                Uri uri = result.getData().getData();
-                try {
-                    InputStream is = getContentResolver().openInputStream(uri);
-                    bitmap = BitmapFactory.decodeStream(is);
-                    img.setImageBitmap(bitmap);
-                } catch (Exception e) {
-                    Log.e(e.toString(), e.getMessage());
-                }
-            }
-        }
-    });
 
-    public void SelectPhoto(View v){
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        pickImg.launch(intent);
-    }
 
     public void Update(View v){
-        CheckFieldsClass checkFieldsClass = new CheckFieldsClass(UpdateData.this);
-        if(checkFieldsClass.IsClear(Name.getText().toString(), Country.getText().toString(), Year.getText().toString())){
-            film.setNameFilm(Name.getText().toString());
-            film.setCountry(Country.getText().toString());
-            film.setReleaseYear(Integer.parseInt(Year.getText().toString()));
-            EncodeImageClass encodeImageClass = new EncodeImageClass();
-            film.setImageFilm(encodeImageClass.Image(bitmap));
-            DataPutDelete(film, 0, "Данные успешно изменены", v);
-        }
+
+            DataPutDelete(osozn, 0, "Данные успешно изменены", v);
+
     }
 
     public void Delete(View v){
-        DataPutDelete(film, 1, "Запись успешно удалена", v);
+        DataPutDelete(osozn, 1, "Запись успешно удалена", v);
     }
 
     public void GoViewData(View v){
         startActivity(new Intent(this, MainActivity.class));
     }
 
-    private void DataPutDelete(Films film, int number, String str, View v) {
+    private void DataPutDelete(Osozn osozn, int number, String str, View v) {
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://ngknn.ru:5001/ngknn/РомановаЕА/practika/api/")
+                .baseUrl("https://ngknn.ru:5001/ngknn/БыковаАА/Api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-        Call<Films> call = null;
+        Call<Osozn> call = null;
         switch (number) {
             case 0:
                 RetrofitApiPut retrofitAPI = retrofit.create(RetrofitApiPut.class);
-                call = retrofitAPI.createPut(film, film.getIdFilm());
+                call = retrofitAPI.createPut(osozn, osozn.getID());
                 break;
             case 1:
                 RetrofitApiDelete retrofitAPIs = retrofit.create(RetrofitApiDelete.class);
-                call = retrofitAPIs.createDelete(film.getIdFilm());
+                call = retrofitAPIs.createDelete(osozn.getID());
                 break;
             default:
                 break;
         }
-        call.enqueue(new Callback<Films>() {
+        call.enqueue(new Callback<Osozn>() {
             @Override
-            public void onResponse(Call<Films> call, Response<Films> response) {
+            public void onResponse(Call<Osozn> call, Response<Osozn> response) {
                 Toast.makeText(UpdateData.this, str, Toast.LENGTH_SHORT).show();
                 GoViewData(v);
             }
             @Override
-            public void onFailure(Call<Films> call, Throwable t) {
+            public void onFailure(Call<Osozn> call, Throwable t) {
                 Toast.makeText(UpdateData.this, "Ошибка!", Toast.LENGTH_SHORT).show();
             }
         });
